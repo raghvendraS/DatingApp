f334d129f51984864faf65eb4afe8677f9846699
raghvendraS <raghvendra.mask@gmail.com>
2021-09-07 20:38:00 +0530

    first commit

---
+++ datingApp/Controllers/UsersController.cs
@@ -0,0 +1,38 @@
+﻿using datingApp.Data;
+using datingApp.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace datingApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+
+        private readonly DataContext _conxt;
+        public UsersController(DataContext context  )
+        {
+            this._conxt = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult <IEnumerable<AppUser>>> GetUsers()
+        {
+            return await _conxt.Users.ToListAsync();
+            
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<AppUser>> GetUsers(int id)
+        {
+           return await _conxt.Users.FindAsync(id);
+            
+        }
+    }
+}
---
+++ datingApp/Data/DataContext.cs
@@ -0,0 +1,18 @@
+﻿using datingApp.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace datingApp.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext( DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<AppUser> Users { get; set; }
+    }
+}
